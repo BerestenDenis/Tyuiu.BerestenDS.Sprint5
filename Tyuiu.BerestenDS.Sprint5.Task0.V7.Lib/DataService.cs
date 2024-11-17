26a7f463e66e9831442f611d29640d5dcd1ed27f
Денис Берестень <d.blyl1337@gmail.com>
2024-11-17 22:59:42 +0500

    Task0

--- Tyuiu.BerestenDS.Sprint5.Task0.V7.Lib/DataService.cs
+++ Tyuiu.BerestenDS.Sprint5.Task0.V7.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.BerestenDS.Sprint5.Task0.V7.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
-            double fx = Math.Pow(-x,3)+ 4 * Math.Pow(x,2) - 3/2 * x;
+            double fx = -Math.Pow(x,3)+ 4 * Math.Pow(x,2) - 3/2 * x;
             fx = Math.Round(fx, 3);
             File.WriteAllText(path,Convert.ToString(fx));
             return path;
