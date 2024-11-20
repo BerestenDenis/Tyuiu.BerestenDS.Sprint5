2eec9ca64fb55e42f8d49e9e11f183f4a06d4023
Денис Берестень <d.blyl1337@gmail.com>
2024-11-20 18:10:06 +0500

    Task3

--- Tyuiu.BerestenDS.Sprint5.Task3.V28.Lib/DataService.cs
+++ Tyuiu.BerestenDS.Sprint5.Task3.V28.Lib/DataService.cs
@@ -6,17 +6,19 @@ namespace Tyuiu.BerestenDS.Sprint5.Task3.V28.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
-            double bg = -(1 / 4) * (Math.Pow(x, 3) - (3 * (Math.Pow(x, 2)) + 4));
-            bg = Math.Round(bg,3);
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.bin" });
+
+            double res = 0;
+
+            res = Math.Round(-(1/4)*(Math.Pow(x,3)-3*Math.Pow(x,2)+4), 3);
+
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
             {
-                writer.Write(BitConverter.GetBytes(bg));
+                writer.Write(BitConverter.GetBytes(res));
             }
 
             return path;
-
-
         }
+    }
     }
 }
