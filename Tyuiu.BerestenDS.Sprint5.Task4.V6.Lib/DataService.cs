c6e1dbd36ae56edd3b0a20a9ff63d496813cb923
Денис Берестень <d.blyl1337@gmail.com>
2024-11-22 00:18:49 +0500

    Task4

--- Tyuiu.BerestenDS.Sprint5.Task4.V6.Lib/DataService.cs
+++ Tyuiu.BerestenDS.Sprint5.Task4.V6.Lib/DataService.cs
@@ -6,8 +6,8 @@ namespace Tyuiu.BerestenDS.Sprint5.Task4.V6.Lib
         public double LoadFromDataFile(string path)
         {
             string strX = File.ReadAllText(path);
+            strX = strX.Replace(".", ",");
             double res = Math.Round(1 / Math.Cos(Convert.ToDouble(strX)) + 2.2 * Math.Pow(Convert.ToDouble(strX), 2), 3);
-            double.Parse("52.8725945", System.Globalization.CultureInfo.InvariantCulture);
             return res;
         }
     }
