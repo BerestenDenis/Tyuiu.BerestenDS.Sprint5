cd865ad840d3351cc7e63cc945236a3438d1cc1e
Денис Берестень <d.blyl1337@gmail.com>
2024-11-22 00:13:33 +0500

    Task4

---
+++ Tyuiu.BerestenDS.Sprint5.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.BerestenDS.Sprint5.Task4.V6.Lib;
+namespace Tyuiu.BerestenDS.Sprint5.Task4.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask4V6.txt";
+
+            FileInfo fl = new FileInfo(path);
+            bool fle = fl.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fle);
+        }
+    }
+}
---
+++ Tyuiu.BerestenDS.Sprint5.Task4.V6/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BerestenDS.Sprint5.Task4.V6.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        string path = @"C:\DataSprint5\InPutDataFileTask4V6.txt";
+        Console.WriteLine("Данные находятся в файле : "+path);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
