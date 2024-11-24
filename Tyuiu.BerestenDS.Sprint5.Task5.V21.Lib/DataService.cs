682be54b6db01cec81b9f67a44a1b8d4d7ab2539
Денис Берестень <d.blyl1337@gmail.com>
2024-11-24 23:11:14 +0500

    Task5

--- Tyuiu.BerestenDS.Sprint5.Task5.V21.Lib/DataService.cs
+++ Tyuiu.BerestenDS.Sprint5.Task5.V21.Lib/DataService.cs
@@ -6,13 +6,13 @@ namespace Tyuiu.BerestenDS.Sprint5.Task5.V21.Lib
         public double LoadFromDataFile(string path)
         {
             double res = 1;
-            using (StreamReader reader = new StreamReader(path))
+            using (StreamReader reader = new(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
                     line = line.Replace('.', ',');
-                    if (Convert.ToDouble(line) % 2 == 0 || Convert.ToDouble(line) > 0);
+                    if ( Convert.ToDouble(line) % 2 == 0 || (Convert.ToDouble(line) > 0));
                     {
                         res = 1;
 
