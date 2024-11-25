768517f38f7ca06a73433fe6abca3fa16ebda0eb
Денис Берестень <d.blyl1337@gmail.com>
2024-11-25 13:14:35 +0500

    Task5

--- Tyuiu.BerestenDS.Sprint5.Task5.V21.Lib/DataService.cs
+++ Tyuiu.BerestenDS.Sprint5.Task5.V21.Lib/DataService.cs
@@ -6,45 +6,21 @@ namespace Tyuiu.BerestenDS.Sprint5.Task5.V21.Lib
         public double LoadFromDataFile(string path)
         {
             double res = 1;
-            int maxEven = 0;
-            string strx = File.ReadAllText(path);
-            strx = strx.Replace(".", ",");
-            // Читаем файл и обрабатываем строки
-            foreach (var line in File.ReadLines(strx)) 
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                double x;
+                while ((line = reader.ReadLine()) != null)
                 {
-                    // Разбиваем строку на числа
-                    var numbers = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
-                                      .Select(x =>
-                                      {
-                                          // Пробуем преобразовать каждую строку в число
-                                          if (double.TryParse(x, out double number))
-                                          {
-                                              // Округляем до трёх знаков после запятой
-                                              return Math.Round(number, 3);
-                                          }
-                                          return double.NaN; // Если не удалось преобразовать, возвращаем NaN
-                                      })
-                                      .Where(x => !double.IsNaN(x)); // Убираем NaN
-
-                    // Находим максимальное чётное целое число
-                    foreach (var num in numbers)
+                    x = Convert.ToDouble(line);
+                    if (Math.Abs(x) >= 10 && Math.Abs(x) <= 99)
                     {
-                        if (num % 2 == 0 && num % 1 == 0) // Проверяем, что число целое и делится на 2
-                        {
-                            maxEven = Math.Max(maxEven, (int)num);
-                        }
+                        res *= x;
                     }
                 }
-            static long Factorial(int n)
-            {
-                long res = 1;
-                for (int i = 2; i <= n; i++)
-                {
-                    res *= i;
-                }
-                return res;
             }
-            return res;
+
+            return Math.Round(res, 3);
         }
     }
 }
